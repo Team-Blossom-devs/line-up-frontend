2e2a1a8cc4f274ff1f03b13df1026a0e751efb9c
hyeyeonismm <hyeyeonismm@gmail.com>
2024-04-26 00:00:43 +0900

    feat: 헤더 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import ViewAll from './pages/ViewAll/ViewAll'
 
 export default function Router() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/viewAll" element={<ViewAll />} />
       </Routes>
     </>
   )
