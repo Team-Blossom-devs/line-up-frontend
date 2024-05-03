ccc3dcac7b6e6fd81c07de462a934ed55adb32a8
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-05-03 16:37:49 +0900

    Fix: Mobile ui for admin list page

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -4,28 +4,28 @@ import { Title } from "@/components/Queue/QueueComponent"
 
 export const Admin = () => {
   return (
-    <div className="p-20">
+    <div className="flex flex-col items-center">
       <Logo />
-      <Title title="관리자 페이지" className="text-center text-btn-pink my-10" />
-      <Title title="컴공 ㅇㅇㅇ 주점 현황표" className="my-10" />
-      <div className="flex justify-between font-bold">
+      <Title title="관리자 페이지" className="text-btn-pink" />
+      <Title title="컴공 ㅇㅇㅇ 주점 현황표" className="text-xl my-10" />
+      <div className="w-11/12 flex text-sm justify-between font-bold whitespace-nowrap">
         <p>총 대기 중인 팀 수: {20}팀</p>
         <p>가용 테이블 현황: {3}</p>
       </div>
-      <table className="w-full text-center">
+      <table className="w-11/12 text-center">
         <thead>
-          <tr>
-            <td className="w-32 py-5">이름</td>
-            <td className="w-32">인원</td>
-            <td className="w-32">전화번호</td>
-            <td className="w-32">이용 시간</td>
-            <td className="w-32">입장 상태</td>
+          <tr className="text-sm">
+            <td className="w-2/10 py-5 whitespace-nowrap">이름</td>
+            <td className="w-1/10 whitespace-nowrap">인원</td>
+            <td className="w-3/10 whitespace-nowrap">전화번호</td>
+            <td className="w-3/10 whitespace-nowrap">이용 시간</td>
+            <td className="w-1/10 whitespace-nowrap">입장 상태</td>
           </tr>
         </thead>
         <tbody>
-          <Row id="1" name="userName" count={10} time="1시간 20분" phone="123456" status="enter" />
-          <Row id="1" name="userName" count={10} time="1시간 20분" phone="123456" status="ready" />
-          <Row id="1" name="userName" count={10} time="1시간 20분" phone="123456" status="ing" />
+          <Row id="1" name="userName" count={10} time="1시간 20분" phone="010-1234-5678" status="enter" />
+          <Row id="1" name="userName" count={10} time="1시간 20분" phone="010-1234-5678" status="ready" />
+          <Row id="1" name="userName" count={10} time="1시간 20분" phone="010-1234-5678" status="ing" />
         </tbody>
       </table>
     </div>
