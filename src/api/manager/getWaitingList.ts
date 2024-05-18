18d6181f26efdbcab154fc64181abbce8b563351
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-05-18 10:59:59 +0900

    Fix: getwaitinglist cursor

--- src/api/manager/getWaitingList.ts
+++ src/api/manager/getWaitingList.ts
@@ -1,12 +1,15 @@
 import axios from "axios"
 
 export const getWaitingList = async (size: number, cursor: string) => {
-  const response = await axios.get(`${import.meta.env.VITE_APP_BACKEND_ADDRESS}/api/manager/waitings`, {
+  const response = await axios.get(`${import.meta.env.VITE_APP_BACKEND_ADDRESS}/api/manager/waitings`, cursor ? {
     params: {
       size: size,
       cursor: cursor
     }
+  } : {
+    params: {
+      size: size
+    }
   })
-
   return response.data;
 }
