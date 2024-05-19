56f33516424b0b311ad71c9e6a9eba183394b738
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-05-20 01:54:40 +0900

    Fix: login body data

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
-import { Admin } from './pages/Admin/Admin'
 import { ViewAll } from './pages/ViewAll/ViewAll'
-import AdminLogin from './pages/Admin/Login'
 import { WaitingFunnel } from "./pages/Waiting/WaitingFunnel"
 import { Auth } from './pages/Home/Auth'
 import { Signup } from './pages/Home/Signup'
+import { AdminFunnel } from "./pages/Admin/AdminFunnel"
 
 export default function Router() {
   return (
@@ -13,10 +12,10 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/waiting/:id" element={<WaitingFunnel />} />
-        <Route path="/admin" element={<Admin />} />
         <Route path="/viewAll" element={<ViewAll />} />
         <Route path="/oauth" element={<Auth />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminFunnel />} />
+        <Route path="/admin/login" element={<AdminFunnel />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
--- src/pages/Admin/Login.tsx
+++ src/pages/Admin/Login.tsx
@@ -1,14 +1,15 @@
 import { Logo } from '@/components/Logo/Logo'
 import { LoginForm } from '@/components/Login/LoginForm'
+import { ContextType } from "./AdminFunnel"
 
-function Login() {
+function Login({ roleContext }: { roleContext: React.Context<ContextType | null> }) {
   return (
     <>
       <div className="container mt-20 items-center">
         <Logo />
         <div className="my-5 font-bold text-2xl md:text-3xl">관리자 로그인</div>
         <div className="text-input-text">부여받으신 회원정보를 통해 로그인해주세요</div>
-        <LoginForm />
+        <LoginForm roleContext={roleContext} />
       </div>
     </>
   )
