4f0267e266042e9158339b15fac08749bb03b43d
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-05-20 01:53:24 +0900

    Fix: getSearch uri endwith "/"

--- src/api/search/getSearch.ts
+++ src/api/search/getSearch.ts
@@ -1,10 +1,13 @@
 import axios from 'axios'
 
 export const getSearch = async (searchTerm: string, pageNum: number) => {
-  const response = await axios.get(`${import.meta.env.VITE_APP_BACKEND_ADDRESS}/api/organization/`, {
+  const response = await axios.get(`${import.meta.env.VITE_APP_BACKEND_ADDRESS}/api/organization`, {
     params: {
       searchTerm,
       pageNum,
+    },
+    headers: {
+      'Authorization': `${localStorage.getItem('token')}`,
     },
   })
 
